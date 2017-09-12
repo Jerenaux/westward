e9a2355724081d9de4b8a1409f0dbc3dd7b6598e
Jerome <jerome.renaux@gmail.com>
2017-09-12 22:40:43 +0200

    Successful scrolling between two chunks with Phaser

--- client/phaser_map.js
+++ client/phaser_map.js
@@ -4,7 +4,7 @@
 
 var config = {
     type: Phaser.WEBGL,
-    width: 34*32,
+    width: 33*32,
     height: 20*32,
     parent: 'game',
     scene: {
@@ -17,13 +17,17 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload(){
-    this.load.json('chunk','assets/maps/pqchunks/chunk0.json');
+    this.load.json('chunk0','assets/maps/pqchunks_flat/chunk0.json');
+    this.load.json('chunk1','assets/maps/pqchunks_flat/chunk1.json');
     this.load.image('tileset','assets/tilesets/tilesheet.png');
 }
 
 function create(){
-    console.log(this.cache.json.get('chunk'));
-    var layerData = this.cache.json.get('chunk').layers;
+    game.sx = 0;
+    game.layers = [];
+    var layerData = this.cache.json.get('chunk0').layers;
+    game.nextData = this.cache.json.get('chunk1').layers;
+    console.log(game.nextData);
     for(var i = 0; i < layerData.length; i++){
         if(!layerData[i].data) continue;
         var mapConfig = {
@@ -40,11 +44,65 @@ function create(){
                 texture: 'tileset'
             }
         };
-        game.map = this.make.tilemap(mapConfig);
+        game.layers[i] = this.make.tilemap(mapConfig);
     }
-    //this.cameras.main.setBounds(0,0,34*32,20*32);
+    game.dist = -1;
+
+    //this.cameras.main.setBounds(0,0,5*32,5*32);
+    //setInterval(step,500);
+    //setTimeout(step,500);
+}
+
+function step(){
+    game.sx += 32;
+    if(game.sx == 32){
+        game.dist++;
+        for (var x = 0; x < 34; x++)
+        {
+            for (var y = 0; y < 20; y++)
+            {
+                for(var l = 0; l < game.layers.length; l++) {
+                    var tile = game.layers[l].getTileAt(x, y);
+                    if(x == 33){
+                        var v = (y*34)+(game.dist%34);
+                        var id = game.nextData[l].data[v];
+                        //if(id > 0) tile.setId(id-1);
+                        tile.setId(id > 0 ? id-1 : 473);
+                    }else{
+                        var next = game.layers[l].getTileAt((x+1)%34, y);
+                        if(next.id > 0) tile.setId(next.id);
+                    }
+                }
+            }
+        }
+        game.sx = 0;
+    }
+
 }
 
 function update(){
-
+    game.sx += 2;
+    if(game.sx == 32){
+        game.dist++;
+        for (var x = 0; x < 34; x++)
+        {
+            for (var y = 0; y < 20; y++)
+            {
+                for(var l = 0; l < game.layers.length; l++) {
+                    var tile = game.layers[l].getTileAt(x, y);
+                    if(x == 33){
+                        var v = (y*34)+(game.dist%34);
+                        var id = game.nextData[l].data[v];
+                        //if(id > 0) tile.setId(id-1);
+                        tile.setId(id > 0 ? id-1 : 473);
+                    }else{
+                        var next = game.layers[l].getTileAt((x+1)%34, y);
+                        if(next.id > 0) tile.setId(next.id);
+                    }
+                }
+            }
+        }
+        game.sx = 0;
+    }
+    this.cameras.main.scrollX = game.sx;
 }
