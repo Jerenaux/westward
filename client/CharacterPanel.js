4e42fe801844ee35c420759473ab7b9614fc62dc
Jerome Renaux <jerome.renaux@gmail.com>
2018-01-07 19:13:10 +0100

    Begun working on character panel

---
+++ client/CharacterPanel.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jeren on 07-01-18.
+ */
+
+function CharacterPanel(x,y,width,height,title){
+    Panel.call(this,x,y,width,height,title);
+    this.addInterface();
+}
+
+CharacterPanel.prototype = Object.create(Panel.prototype);
+CharacterPanel.prototype.constructor = CharacterPanel;
