ca870dea42be59f78c1c10e617a64337c3aec413
Mihail Ilinov <mihail@ilinov.eu>
2019-03-21 22:32:13 +0200

    Separate game-server (game), admin-panel, auth server (keycloak)

--- keycloak-5.0.0/bin/kcadm.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-if "%OS%" == "Windows_NT" (
-  set "DIRNAME=%~dp0%"
-) else (
-  set DIRNAME=.\
-)
-java %KC_OPTS% -cp %DIRNAME%\client\keycloak-admin-cli-5.0.0.jar org.keycloak.client.admin.cli.KcAdmMain %*
